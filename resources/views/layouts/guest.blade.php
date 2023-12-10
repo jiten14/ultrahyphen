d0566efbd70b23d6b3739f3699978f9a5c16b9a3
jiten14 <jitendriya14@gmail.com>
2023-12-10 09:23:17 +0530

    Minor Changes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -54,27 +54,17 @@
         <ul>
           <li><a href="index.html">Blog</a></li>
           <li><a href="single-post.html">Single Post</a></li>
-          <li class="dropdown"><a href="category.html"><span>Categories</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-            <ul>
-              <li><a href="search-result.html">Search Result</a></li>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-
-          <li><a href="about.html">About</a></li>
-          <li><a href="contact.html">Contact</a></li>
+          
+          @if (Route::has('login'))
+            @auth
+              <li><a href="{{ url('/dashboard') }}" class="getstarted">Dashboard</a></li>
+            @else
+              <li><a href="{{ route('login') }}" class="getstarted">Login</a></li>
+              @if (Route::has('register'))
+                <li><a href="{{ route('register') }}" class="getstarted">Register</a></li>
+              @endif
+            @endauth
+          @endif
         </ul>
       </nav><!-- .navbar -->
       <i class="bi bi-list mobile-nav-toggle"></i>
@@ -96,20 +86,6 @@
         </div><!-- End Search Form -->
 
       </div>--}}
-
-      @if (Route::has('login'))
-        <div class="position-relative">
-          @auth
-            <a href="{{ url('/dashboard') }}" class="btn btn-primary">Dashboard</a>
-          @else
-            <a href="{{ route('login') }}" class="btn btn-primary">Login</a>
-            @if (Route::has('register'))
-              <a href="{{ route('register') }}" class="btn btn-secondary">Register</a>
-            @endif
-          @endauth
-          
-        </div>
-      @endif
 
     </div>
 
